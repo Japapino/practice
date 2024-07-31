9c4da29cb831385f24ef101a04003b4cd7c8ea8f
Stefan M <stefan.murakami@gmail.com>
2024-07-31 15:16:13 -0400

    threeSum problem

--- practice1.js
+++ practice1.js
@@ -8,12 +8,12 @@ const readln = readline.createInterface({
 let currentPlayer = "R";
 
 const board = [
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " "],
 ];
 
 function printBoard() {
@@ -38,7 +38,7 @@ function checkWinner() {
         board[i][j] === board[i][j + 2] &&
         board[i][j] === board[i][j + 3]
       ) {
-        console.log('horizontal win');
+        console.log("horizontal win");
         console.log(board[i][j], " wins!");
         return true;
       }
@@ -54,7 +54,7 @@ function checkWinner() {
         board[i][j] === board[i + 2][j] &&
         board[i][j] === board[i + 3][j]
       ) {
-        console.log('vertical win');
+        console.log("vertical win");
         console.log(board[i][j], " wins!");
         return true;
       }
@@ -70,7 +70,7 @@ function checkWinner() {
         board[i][j] === board[i + 2][j + 2] &&
         board[i][j] === board[i + 3][j + 3]
       ) {
-        console.log('diag neg win');
+        console.log("diag neg win");
         console.log(board[i][j], " wins!");
         return true;
       }
@@ -86,7 +86,7 @@ function checkWinner() {
         board[i][j] === board[i - 2][j + 2] &&
         board[i][j] === board[i - 3][j + 3]
       ) {
-        console.log('diag pos win');
+        console.log("diag pos win");
         console.log(board[i][j], " wins!");
         return true;
       }
@@ -97,50 +97,52 @@ function checkWinner() {
 }
 
 function playTurn() {
-  readln.question(`Player ${currentPlayer}, enter your move (column): `, (answer) => {
-    const col = parseInt(answer, 10);
+  readln.question(
+    `Player ${currentPlayer}, enter your move (column): `,
+    (answer) => {
+      const col = parseInt(answer, 10);
 
-    // validate column selection
-    if (isNaN(col) || col < 0 || col > 7 ) {
-      console.log("Invalid move. Outside of game board. Try again.");
-      playTurn();
-      return;
-    } else if (board[0][col] !== " ") {
-      console.log("Invalid move. Column full. Try again.");
-      playTurn();
-      return;
-    }
-
-    // place piece, move bottom up
-    for (let row = 5; row >= 0; row --) {
-      if (board[row][col] === " ") {
-        board[row][col] = currentPlayer;
-        break;
+      // validate column selection
+      if (isNaN(col) || col < 0 || col > 7) {
+        console.log("Invalid move. Outside of game board. Try again.");
+        playTurn();
+        return;
+      } else if (board[0][col] !== " ") {
+        console.log("Invalid move. Column full. Try again.");
+        playTurn();
+        return;
       }
-    }
 
-    printBoard();
+      // place piece, move bottom up
+      for (let row = 5; row >= 0; row--) {
+        if (board[row][col] === " ") {
+          board[row][col] = currentPlayer;
+          break;
+        }
+      }
 
-    if (checkWinner()) {
-      readln.close();
-      return;
-    }
-    
-    if(isBoardFull()) {
-      readln.close();
-      return;
-    }
+      printBoard();
 
-    switchPlayer();
-    playTurn();
-  });
+      if (checkWinner()) {
+        readln.close();
+        return;
+      }
+
+      if (isBoardFull()) {
+        readln.close();
+        return;
+      }
+
+      switchPlayer();
+      playTurn();
+    }
+  );
 }
 
 function isBoardFull() {
   for (let i = 0; i < 6; i++) {
     for (let j = 0; j < 7; j++) {
-      if (
-        board[i][j] == " ") {
+      if (board[i][j] == " ") {
         return false;
       }
     }
@@ -149,8 +151,44 @@ function isBoardFull() {
   return true;
 }
 
-printBoard();
-playTurn();
+function threeSum(nums) {
+  let result = [];
+
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length && i != i-1; i++) {
+    if (nums[i]> 0) break;
+    if (i > 0 && nums[i] == nums[i-1]) continue;
+      let start = i+1; 
+      let end = nums.length-1;
+      while (start < end) {
+        let sum = nums[i] + nums[start] + nums[end];
+        if (sum == 0) {
+          result.push([nums[i], nums[start], nums[end]]);
+
+          start++;
+          end--;
+
+          while(start<end && nums[start]===nums[start-1]) start++;
+          while(start<end && nums[end]===nums[end+1]) end--;
+
+        } else if (sum < 0) {
+          start++;
+        } else if (sum > 0 ) {
+          end--; 
+        }
+      }
+    }
+
+  return result;
+}
+
+// let nums=[-1,0,1,2,-1,-4];
+let nums=[1,-1,-1,0];
+console.log(threeSum(nums));
+
+
+// printBoard();
+// playTurn();
 
 // horizontalTestBoard
 // const board = [
